8328a8c99bbcc409b99e3f03301fc4e7f2f3762a
fhur <fhurtado@twilio.com>
2015-03-01 20:40:00 -0800

    ADded documentation to Lexer

--- lib/twirlio/lexer.rb
+++ lib/twirlio/lexer.rb
@@ -1,5 +1,43 @@
+#
+# Supported tokens
+#
+# Keywords
+#
+# [:SEND, 'send']
+# [:READ, 'read']
+# [:EXIT, 'exit']
+# [:IF, 'if']
+#
+# Special characters
+#
+# ['(','(']
+# [')',')']
+#
+# Comparators
+#
+# [:COMPARATOR, '=']
+# [:COMPARATOR, '<']
+# [:COMPARATOR, '>']
+#
+# Identifiers
+#
+# [:IDEN, any_lower_case_words]
+#
+# Strings
+#
+# [:STRING, string] matches anything surrounded by " or '. Currently does not allow scaping
+#
+# Skips
+# This lexer will skip any line that starts with the '#' character (comments), whitespace
+# are skipped and line breaks are skipped.
+#
+#
 class Lexer
 
+  # @param {string} code a twisp program
+  # @return a list a tokens where every token is an array with two values, the
+  #         first one is the token identifier and the second one is the token's value.
+  #
   def tokenize(code)
     code.chomp!
     tokens = []
@@ -9,21 +47,19 @@ class Lexer
     while i < code.size
       chunk = code[i..-1]
 
+      # match comments
+      if comment = chunk[/\A(#.*$)/,1]
+        i += comment.size
 
       # match parenthesis
-      if parens = chunk[/\A(\(|\))/,1]
+      elsif parens = chunk[/\A(\(|\))/,1]
         tokens << [parens, parens]
         i += parens.size
 
-      # commands (send, read, exit)
-      elsif command = chunk[/\A(send|read|exit)/,1]
+      # commands (send, read, exit, if)
+      elsif command = chunk[/\A(send|read|exit|if)/,1]
         tokens << [command.upcase.to_sym, command]
         i += command.size
-
-      # 'if' keyword
-      elsif ifkw = chunk[/\A(if)/,1]
-        tokens << [ifkw.upcase.to_sym, ifkw]
-        i += ifkw.size
 
       # comparators
       elsif comparator = chunk[/\A(=|<|>)/,1]
